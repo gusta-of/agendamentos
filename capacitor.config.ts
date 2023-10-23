01d8b501a18a560798f2e8b333580a786caf5c62
Gustavo Fernandes <gustavoofernandes9712@gmail.com>
2023-10-23 09:34:31 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'agendamentos',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
