c39cbf3dfdc3de35b74a1ba054f6efe7de95209e
Gustavo Fernandes <gustavoofernandes9712@gmail.com>
2023-10-23 12:03:24 -0300

    cria eventos exemplo de anipulação do calendário para criar a agenda

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
+  calendarOptions: CalendarOptions = {};
   constructor() {}
 
+  ngOnInit(): void {
+    this.calendarOptions = this.data();
+  }
+
+  data(): CalendarOptions {
+    return {
+      initialView: 'dayGridMonth',
+      locale: ['pt'],
+      dateClick: this.handleDateClick.bind(this),
+      datesSet: this.drop.bind(this),
+      plugins: [dayGridPlugin, interactionPlugin],
+      showNonCurrentDates: false,
+      contentHeight: 300,
+      hiddenDays: [20, 21],
+      events: [
+        {
+          start: '2023-10-24',
+          end: '2023-10-24',
+          display: 'background',
+          backgroundColor: 'red',
+        },
+      ],
+      buttonText: {
+        today: 'Hoje'
+      }
+    }
+  }
+
+  handleDateClick(arg: any) {
+    alert('date click! ' + arg.dateStr)
+  }
+
+  drop(args: any) {
+    alert(`Month: ${args.start.getMonth() + 1}`);
+  }
 }
